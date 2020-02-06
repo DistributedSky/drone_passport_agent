c580c3944b6a589c0ef4474d180a9f7a786cbb58
Vadim Manaenko <vadim.razorq@gmail.com>
2020-02-06 15:38:04 +0300

    Add pinata pinning. 0.2.4

--- default.nix
+++ default.nix
@@ -7,11 +7,15 @@
 mkRosPackage rec {
   name = "${pname}-${version}";
   pname = "drone_passport_agent";
-  version = "0.2.3";
+  version = "0.2.4";
 
   src = ./.;
 
-  propagatedBuildInputs = [ robonomics_comm pkgs.python37Packages.flask-restful ];
+  propagatedBuildInputs = [
+    robonomics_comm
+    pkgs.python37Packages.flask-restful
+    pkgs.python37Packages.pinatapy
+  ];
 
   meta = with stdenv.lib; {
     description = "DE direct";
